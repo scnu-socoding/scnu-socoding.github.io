d1263743d742c202eb7de77e1e5de8c11dafff9f
HK-SHAO <hk-shao@foxmail.com>
2022-08-25 11:49:03 +0800

    add oj&cloud

--- src/pages/links.vue
+++ src/pages/links.vue
@@ -5,6 +5,20 @@ const { t } = useI18n()
 
 const data: CardData[] = [
   {
+    title: 'SoCoding OJ',
+    description: 'SoCoding 在线评测系统',
+    action: [
+      { label: 'Go', icon: 'i-ri-links-fill', url: '//oj.socoding.cn' },
+    ],
+  },
+  {
+    title: 'SoCoCloud',
+    description: 'SoCoding 在线存储服务',
+    action: [
+      { label: 'Go', icon: 'i-ri-links-fill', url: '//cloud.socoding.cn' },
+    ],
+  },
+  {
     title: '这是一个友联',
     description: '欢迎加入我们的友联，通过 board@socoding.cn 联系我们',
     action: [
@@ -16,11 +30,7 @@ const data: CardData[] = [
 
 <template>
   <Card
-    v-for="(it, i) in data"
-    :key="i"
-    :title="t(it.title)"
-    :description="t(it.description)"
-    :action="it.action"
+    v-for="(it, i) in data" :key="i" :title="t(it.title)" :description="t(it.description)" :action="it.action"
     :delay="i"
   />
 </template>
