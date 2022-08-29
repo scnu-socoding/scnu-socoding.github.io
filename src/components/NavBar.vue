bc8bf33188987895acee0c5212b56775d1b4b349
HK-SHAO <hk-shao@foxmail.com>
2022-08-29 23:52:58 +0800

    fix tooltips

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -13,54 +13,58 @@ const toggleLocales = () => {
 <template>
   <nav text-xl>
     <ToolTipsProvider>
-      <animate-component duration-300 :delay="0">
-        <ToolTip :description="t('button.home')">
-          <RouterLink class="icon-btn mx-2" to="/" :title="t('button.home')">
+      <ToolTip :description="t('button.home')">
+        <RouterLink class="icon-btn mx-2" to="/" :title="t('button.home')">
+          <animate-component duration-300 :delay="0">
             <div i-carbon-home />
-          </RouterLink>
-        </ToolTip>
-      </animate-component>
-      <animate-component duration-300 :delay="1">
-        <ToolTip :description="t('button.docs')">
-          <RouterLink class="icon-btn mx-2" to="/docs" :title="t('button.links')">
+          </animate-component>
+        </RouterLink>
+      </ToolTip>
+      <ToolTip :description="t('button.docs')">
+        <RouterLink class="icon-btn mx-2" to="/docs" :title="t('button.links')">
+          <animate-component duration-300 :delay="1">
             <div i-carbon-book />
-          </RouterLink>
-        </ToolTip>
-      </animate-component>
-      <animate-component duration-300 :delay="2">
-        <ToolTip :description="t('button.links')">
-          <RouterLink class="icon-btn mx-2" to="/links" :title="t('button.links')">
+          </animate-component>
+        </RouterLink>
+      </ToolTip>
+
+      <ToolTip :description="t('button.links')">
+        <RouterLink class="icon-btn mx-2" to="/links" :title="t('button.links')">
+          <animate-component duration-300 :delay="2">
             <div i-carbon-link />
-          </RouterLink>
-        </ToolTip>
-      </animate-component>
-      <animate-component duration-300 :delay="3">
-        <ToolTip :description="t('button.about')">
-          <RouterLink class="icon-btn mx-2" to="/about" :title="t('button.about')">
+          </animate-component>
+        </RouterLink>
+      </ToolTip>
+
+      <ToolTip :description="t('button.about')">
+        <RouterLink class="icon-btn mx-2" to="/about" :title="t('button.about')">
+          <animate-component duration-300 :delay="3">
             <div i-carbon-information />
-          </RouterLink>
-        </ToolTip>
-      </animate-component>
-      <animate-component duration-300 :delay="4">
-        <!-- <ToolTip :description="t('button.tools')">
+          </animate-component>
+        </RouterLink>
+      </ToolTip>
+
+      <!-- <ToolTip :description="t('button.tools')">
         <RouterLink class="icon-btn mx-2" to="/tools" :title="t('button.tools')">
           <div i-carbon-tool-kit />
         </RouterLink>
       </ToolTip> -->
 
-        <ToolTip :description="t('button.toggle_dark')">
-          <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
+      <ToolTip :description="t('button.toggle_dark')">
+        <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
+          <animate-component duration-300 :delay="4">
             <div i="carbon-sun dark:carbon-moon" />
-          </button>
-        </ToolTip>
-      </animate-component>
-      <animate-component duration-300 :delay="5">
-        <ToolTip :description="t('button.toggle_langs')">
-          <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales">
+          </animate-component>
+        </button>
+      </ToolTip>
+
+      <ToolTip :description="t('button.toggle_langs')">
+        <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales">
+          <animate-component duration-300 :delay="5">
             <div i-carbon-language />
-          </a>
-        </ToolTip>
-      </animate-component>
+          </animate-component>
+        </a>
+      </ToolTip>
     </ToolTipsProvider>
   </nav>
 </template>
