0ef6f21b83ce4997594db130a9c8e979c2eeebdc
HK-SHAO <hk-shao@foxmail.com>
2022-09-04 23:52:11 +0800

    add <a>

--- src/pages/membership.vue
+++ src/pages/membership.vue
@@ -13,7 +13,7 @@ document.title = '成员们 | SoCoding'
   <Accordion :name="'membership'" :delay="2" default-row="core" max-w-5xl ma>
     <AccordionRow name="core" :title="t('membership.core-members')">
       <p color-zinc-300 dark:color-zinc-600 pb-2>
-        按照姓名拼音排序，如有遗漏请联系 hr@socoding.cn
+        按照姓名拼音排序，如有遗漏请联系 <a href="mailto:hr@socoding.cn">hr@socoding.cn</a>
       </p>
       <ToolTipsProvider :gap="1">
         <ToolTip v-for="(it, i) in coreMembers.data" :key="i" :description="it.name">
@@ -23,7 +23,7 @@ document.title = '成员们 | SoCoding'
     </AccordionRow>
     <AccordionRow name="members" :title="t('membership.members')">
       <p color-zinc-300 dark:color-zinc-600 pb-2>
-        按照姓名拼音排序，如有遗漏请联系 hr@socoding.cn
+        按照姓名拼音排序，如有遗漏请联系 <a href="mailto:hr@socoding.cn">hr@socoding.cn</a>
       </p>
       <ToolTipsProvider :gap="1">
         <ToolTip v-for="(it, i) in members.data" :key="i" :description="it.name">
@@ -33,7 +33,7 @@ document.title = '成员们 | SoCoding'
     </AccordionRow>
     <AccordionRow name="partners" :title="t('membership.partners')">
       <p color-zinc-300 dark:color-zinc-600 pb-2>
-        我们很高兴与你们一起，联系 board@socoding.cn
+        我们很高兴与你们一起，联系 <a href="mailto:board@socoding.cn">board@socoding.cn</a>
       </p>
       <ToolTipsProvider :gap="1">
         <ImageCard
