bdaa639c0c79ba391fd44f56d8cb042ca1409301
HK-SHAO <hk-shao@foxmail.com>
2022-09-04 22:28:42 +0800

    mod links

--- src/pages/links.vue
+++ src/pages/links.vue
@@ -7,40 +7,41 @@ document.title = '链接 | SoCoding'
 
 const data: CardData[] = [
   {
-    title: 'SoCoding OJ',
+    title: '软协 | SoCoding OJ',
     description: 'SoCoding 在线评测系统',
     action: [
       { label: 'Go', icon: 'i-ri-links-fill', url: '//oj.socoding.cn' },
     ],
   },
   {
-    title: 'SoCoding Cloud',
+    title: '软协 | SoCoding Cloud',
     description: 'SoCoding 在线存储服务',
     action: [
       { label: 'Go', icon: 'i-ri-links-fill', url: '//cloud.socoding.cn' },
     ],
   },
   {
-    title: 'SoCoding Puzzle',
+    title: '软协 | SoCoding Puzzle',
     description: 'SoCoding 在线解密平台',
     action: [
       { label: 'Go', icon: 'i-ri-links-fill', url: '//puzzle.socoding.cn' },
     ],
   },
   {
-    title: 'SoCoding Status',
+    title: '软协 | SoCoding Status',
     description: 'SoCoding 公网服务状态',
     action: [
       { label: 'Go', icon: 'i-ri-links-fill', url: '//status.socoding.cn' },
     ],
   },
   {
-    title: '这是一个友联',
-    description: '欢迎加入我们的友联，通过 board@socoding.cn 联系我们',
+    title: '友联 | 0xFFFF 搬砖技术社区',
+    description: '这是一个很棒的技术交流平台和学习平台哦~',
     action: [
-      { label: 'Go', icon: 'i-ri-links-fill', url: '' },
+      { label: 'Go', icon: 'i-ri-links-fill', url: '//0xffff.one' },
     ],
   },
+
 ]
 </script>
 
@@ -49,4 +50,6 @@ const data: CardData[] = [
     v-for="(it, i) in data" :key="i" :title="t(it.title)" :description="t(it.description)" :action="it.action"
     :delay="i"
   />
+
+  欢迎加入我们的友联，通过 <a href="mailto:board@socoding.cn">board@socoding.cn</a> 联系我们
 </template>
