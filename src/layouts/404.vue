934df898710d3f6de0f8d8b625ceeb9b83642baf
HK-SHAO <hk-shao@foxmail.com>
2022-09-06 14:58:01 +0800

    fix cannot download pdf && add file download page

--- src/layouts/404.vue
+++ src/layouts/404.vue
@@ -5,6 +5,36 @@ const { t } = useI18n()
 function refresh() {
   window.location.replace(`${window.location.pathname}?t=${Date.now()}`)
 }
+
+const fileName = location.pathname.split('/').pop()
+const isFile = fileName && fileName.toLowerCase().endsWith('.pdf')
+
+if (isFile)
+  document.title = `${fileName} | SoCoding`
+else
+  document.title = '页面找不到啦 | SoCoding'
+
+function downloadThis() {
+  if (isFile) {
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', location.href, true)
+    xhr.responseType = 'blob'
+    xhr.onload = function (e) {
+      if (this.status === 200) {
+        const blob = this.response
+        const a = document.createElement('a')
+        blob.type = 'application/octet-stream'
+        const url = URL.createObjectURL(blob)
+
+        a.href = url
+        a.download = fileName
+        a.click()
+        window.URL.revokeObjectURL(url)
+      }
+    }
+    setTimeout(() => { xhr.send() }, 0)
+  }
+}
 </script>
 
 <template>
@@ -13,16 +43,17 @@ function refresh() {
       <Header />
       <main p="x4 y10" text="center teal-700 dark:gray-200">
         <div text-4xl max-w-lg ma lg:mt-4rem p-2xl lg:p-0 class="animate-fadein-shift-y--2-1000">
-          <div i-carbon-warning inline-block />
+          <div v-if="!isFile" i-ri-file-warning-line inline-block />
+          <div v-else i-ri-file-line inline-block />
         </div>
         <div class="animate-fadein-shift-y--2-500">
-          {{ t('not-found') }}
+          {{ isFile?fileName:t('not-found') }}
         </div>
         <button btn text-sm m="3 t8" @click="router.back()">
           {{ t('button.back') }}
         </button>
-        <button btn text-sm m="3 t8" @click="refresh()">
-          {{ t('button.refresh') }}
+        <button btn text-sm m="3 t8" @click="isFile?downloadThis():refresh()">
+          {{ isFile?t('button.download'):t('button.refresh') }}
         </button>
         <RouterView />
         <div />
--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-document.title = '页面找不到啦 | SoCoding'
 </script>
 
 <route lang="yaml">
