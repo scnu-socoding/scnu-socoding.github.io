b1b77b98ccd0cc496835a22b948fb26de62cb1d4
HK-SHAO <hk-shao@foxmail.com>
2022-10-24 02:20:26 +0800

    update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,74 +10,55 @@ if (document)
 </script>
 
 <template>
-  <div
-    max-w-full
-    ma lg:mt-4rem
-    :class="(isDark?'dark-filter ':'') + 'bg-img animate-fadein-shift-y--2-1000'"
-  />
-  <br>
+  <div max-w-full ma lg:mt-4rem :class="(isDark ? 'dark-filter ' : '') + 'bg-img animate-fadein-shift-y--2-1000'" />
+  <div style="height: 20pt;" />
 
-  <div
-    text-xl sm:text-2xl lg:text-3xl
-    my-2xl
-    flex justify-center
-    animate-fade-in animate-count-1
-  >
+  <div text-xl sm:text-2xl lg:text-3xl my-2xl flex justify-center animate-fade-in animate-count-1>
     <ToolTipsProvider :gap="6">
-      <ToolTip
-        v-for="(it, i) in links" :key="i"
-        :description="t(it.description)"
-      >
+      <ToolTip v-for="(it, i) in links" :key="i" :description="t(it.description)">
         <animate-component duration-300 :delay="i">
           <a :href="it.url" icon-btn :class="it.icon" />
         </animate-component>
       </ToolTip>
     </ToolTipsProvider>
   </div>
+
+  <span class="flag" />
 </template>
 
 <style scoped>
-
-.dark-filter  {
+.dark-filter {
   filter: invert(0.93) hue-rotate(180deg) !important;
 }
 
 @media screen and (min-width: 1024px) {
   .bg-img {
-      height: 60%;
-      background: url("/logo/socoding.svg") center;
-      background-size: contain;
-      background-repeat: no-repeat;
+    height: 60%;
+    background: url("/logo/socoding.svg") center;
+    background-size: contain;
+    background-repeat: no-repeat;
   }
 
-.chisato{
-  position: fixed;
-  left: 24px;
-  bottom: 24px;
-}
-
-.takina{
-  position: fixed;
-  right: 24px;
-  bottom: 24px;
-}
 }
 
 @media screen and (max-width: 1024px) {
-.bg-img {
+  .bg-img {
     height: 60%;
     background: url("/favicon.svg") center;
     background-size: contain;
     background-repeat: no-repeat;
   }
-
-.chisato{
-  visibility: hidden;
 }
 
-.takina{
-  visibility: hidden;
-}
+.flag::before {
+  content: "big brother is watching you";
+  font: bold 10%/1 monospace;
+  position: fixed;
+  left: 10pt;
+  bottom: 10pt;
+  z-index: -1000;
+  color: #fafafa
+    /* Match the color of the texts w/ background. */
 }
 </style>
 
